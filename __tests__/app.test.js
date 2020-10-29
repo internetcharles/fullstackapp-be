daaafbc615ca69b100dee8ed0998bc357014e4a6
internetcharles <charlesodellsmith@gmail.com>
2020-10-29 15:18:17 -0700

    app test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -5,6 +5,22 @@ const app = require('../lib/app');
 
 describe('fullstackapp-be routes', () => {
   beforeEach(() => {
-    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'))
+    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'));
   });
+
+  it('returns a new studio via POST', () => {
+    return request(app)
+      .post('/api/v1/notes')
+      .send({
+        title: 'hello',
+        note: 'hello'
+      })
+      .then(res => expect(res.body)
+        .toEqual({
+          id: expect.any(String),
+          title: 'hello',
+          note: 'hello'
+        }));
+  });
+
 });
